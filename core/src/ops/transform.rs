3dfaf3a941909a79b6ae529de2eacd45977d5cc2
Joe McCain III <jo3mccain@icloud.com>
2024-07-29 10:50:09 -0500

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- core/src/ops/transform.rs
+++ core/src/ops/transform.rs
@@ -3,14 +3,12 @@
     Contrib: FL03 <jo3mccain@icloud.com>
 */
 
-/// The [`Transform`] trait generically describes objects capable of applying some transformation.
-/// From a "rustic" point of view, the trait simply provides an additional reference point for
-/// the `map` method, which is a method that applies a function to a value and returns a new value.
-///  
-/// [`Transform::transform`] is a method that takes a reference to `self` and a value of type `T`
-/// and returns a value of type [`Transform::Output`].
+/// [`Transform`] is describes a binary operation capable of applying some transformation.
+/// More commonly, the typical "rustic" manner of which an object is transformed is through
+/// the [`map`] method, which applies a function to a value and returns a new value.
 pub trait Transform<T> {
     type Output;
-
+    /// [`Transform::transform`] is a method that takes a reference to `self` and a value of type 
+    /// `T` and returns a value of type [`Transform::Output`].
     fn transform(&self, delta: T) -> Self::Output;
 }
