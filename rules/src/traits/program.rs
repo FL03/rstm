0a237d54574d21923d9604370acc4a194d2b84ed
FL03 <jo3mccain@icloud.com>
2025-08-30 08:46:23 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- rules/src/traits/program.rs
+++ rules/src/traits/program.rs
@@ -6,23 +6,7 @@ use crate::{Head, Rule, Tail};
 use rstm_core::Direction;
 use rstm_state::{RawState, State};
 
-/// The [`Program`] trait establishes a common interface for objects that represent a
-/// collection of rules.
-pub trait RawRuleset<Q, A>
-where
-    Q: RawState,
-{
-    type Key: Scope<Q, A>;
-    type Val: Directive<Q, A>;
 
-    fn contains_key(&self, key: &Self::Key) -> bool;
-
-    fn get(&self, key: &Self::Key) -> Option<&Self::Val>;
-
-    fn insert(&mut self, key: Self::Key, val: Self::Val) -> Option<Self::Val>;
-
-    fn remove(&mut self, key: &Self::Key) -> Option<Self::Val>;
-}
 /// The [`Scope`] trait is used to describe objects containing information or references to the
 /// current state and symbol of a Turing machine.
 pub trait Scope<Q, S> {
@@ -43,72 +27,7 @@ pub trait Directive<Q, S> {
 /*
  ************* Implementations *************
 */
-#[cfg(feature = "alloc")]
-use alloc::vec::Vec;
 
-#[cfg(feature = "std")]
-use std::collections::HashMap;
-
-#[cfg(feature = "alloc")]
-impl<Q, A> RawRuleset<Q, A> for Vec<Rule<Q, A>>
-where
-    Q: RawState + PartialEq,
-    A: PartialEq,
-{
-    type Key = Head<Q, A>;
-    type Val = Tail<Q, A>;
-
-    fn contains_key(&self, key: &Head<Q, A>) -> bool {
-        self.iter().any(|rule| rule.head() == key)
-    }
-
-    fn get(&self, key: &Head<Q, A>) -> Option<&Tail<Q, A>> {
-        self.iter().find_map(|rule| {
-            if rule.head() == key {
-                Some(rule.tail())
-            } else {
-                None
-            }
-        })
-    }
-
-    fn insert(&mut self, key: Head<Q, A>, val: Tail<Q, A>) -> Option<Tail<Q, A>> {
-        self.iter_mut()
-            .find(|rule| rule.head() == &key)
-            .map(|rule| core::mem::replace(rule.tail_mut(), val))
-    }
-
-    fn remove(&mut self, key: &Head<Q, A>) -> Option<Tail<Q, A>> {
-        let index = self.iter().position(|rule| rule.head() == key)?;
-        Some(self.remove(index).tail)
-    }
-}
-
-#[cfg(feature = "std")]
-impl<Q, A> RawRuleset<Q, A> for HashMap<Head<Q, A>, Tail<Q, A>>
-where
-    Q: RawState + Eq + core::hash::Hash,
-    A: Eq + core::hash::Hash,
-{
-    type Key = Head<Q, A>;
-    type Val = Tail<Q, A>;
-
-    fn contains_key(&self, key: &Head<Q, A>) -> bool {
-        self.contains_key(key)
-    }
-
-    fn get(&self, key: &Head<Q, A>) -> Option<&Tail<Q, A>> {
-        self.get(key)
-    }
-
-    fn insert(&mut self, key: Head<Q, A>, val: Tail<Q, A>) -> Option<Tail<Q, A>> {
-        self.insert(key, val)
-    }
-
-    fn remove(&mut self, key: &Head<Q, A>) -> Option<Tail<Q, A>> {
-        self.remove(key)
-    }
-}
 
 impl<Q, S> Scope<Q, S> for (State<Q>, S) {
     fn current_state(&self) -> &State<Q> {
--- rules/src/traits/rulespace.rs
+++ rules/src/traits/rulespace.rs
@@ -19,3 +19,58 @@ where
         Q: PartialEq,
         S: PartialEq;
 }
+
+/*
+ ************* Implementations *************
+*/
+
+#[cfg(feature = "alloc")]
+mod impl_alloc {
+    use super::{RawSpace, RuleSpace};
+    use crate::{Head, Rule, Tail};
+    use alloc::vec::Vec;
+    use rstm_state::RawState;
+
+    impl<T> RawSpace for Vec<T> {
+        seal! {}
+    }
+
+    impl<Q, A> RuleSpace<Q, A> for Vec<Rule<Q, A>>
+    where
+        Q: RawState + PartialEq,
+        A: PartialEq,
+    {
+        fn get(&self, key: &Head<Q, A>) -> Option<&Tail<Q, A>> {
+            self.iter().find_map(|rule| {
+                if rule.head() == key {
+                    Some(rule.tail())
+                } else {
+                    None
+                }
+            })
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+mod impl_std {
+    use super::{RawSpace, RuleSpace};
+
+    use crate::{Head, Tail};
+    use rstm_state::RawState;
+    use std::collections::HashMap;
+
+    impl<K, V> RawSpace for HashMap<K, V> {
+        seal! {}
+    }
+
+    impl<Q, A> RuleSpace<Q, A> for HashMap<Head<Q, A>, Tail<Q, A>>
+    where
+        Q: RawState + Eq + core::hash::Hash,
+        A: Eq + core::hash::Hash,
+    {
+        fn get(&self, key: &Head<Q, A>) -> Option<&Tail<Q, A>> {
+            self.get(key)
+        }
+    }
+}
