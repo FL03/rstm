8b272a4ad123f8679e9e4ed114880b7d18e6995a
FL03 <jo3mccain@icloud.com>
2026-01-14 23:51:49 -0600

    impl_head_ext.rs
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- core/src/cmp/head/impl_head_ext.rs
+++ core/src/cmp/head/impl_head_ext.rs
@@ -14,10 +14,7 @@ where
     A: core::fmt::Debug,
 {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_tuple("Head")
-            .field(&self.state)
-            .field(&self.symbol)
-            .finish()
+        write! { f, "{{ state: {:?}, symbol: {:?} }}", self.state, self.symbol }
     }
 }
 
@@ -27,7 +24,7 @@ where
     A: core::fmt::Display,
 {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "({}, {})", self.state, self.symbol)
+        write! { f, "({}, {})", self.state, self.symbol }
     }
 }
 
