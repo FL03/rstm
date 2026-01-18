615a127400d48e278c6356adfcf481b02cd7ae41
FL03 <jo3mccain@icloud.com>
2026-01-18 00:19:39 -0600

    v0.1.4
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       in rec {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "rstm";
-          version = "0.1.3";
+          version = "0.1.5";
           src = self; # ./.;
           cargoLock = {
             lockFile = ./Cargo.lock;
