72b3bd66e68742a4ff60f2d96e0980cdd1e1b81c
FL03 <joe@pzzld.org>
2026-03-04 19:19:06 -0600

    update nix
    Signed-off-by: FL03 <joe@pzzld.org>

--- scripts/nix.sh
+++ scripts/nix.sh
@@ -2,15 +2,28 @@
 
 # flags for the script:
 # --no-link: don't create a symlink to the result in the current directory
-# --no-build: don't build the package, just print the path to the result
-# -c or --command: run the given command in the shell with the package available (default build)
+# -b or --build: build the package and create a symlink to the result in the current directory (default)
 
 [ "$1" = "--no-link" ] && no_link=true && shift
-[ "$1" = "--no-build" ] && no_build=true && shift
-if [ "$1" = "-c" ] || [ "$1" = "--command" ]; then
-  shift
-  nix --extra-experimental-features 'nix-command flakes' shell  .#rstm --command "$@" --allow-dirty
-  exit $?
+[ "$1" = "-b" ] && build_flag=true && shift
+[ "$1" = "--build" ] && build_flag=true && shift
+
+if [ "$no_link" = true ]; then
+  link_flag="--no-link"
+else
+  link_flag=""
 fi
 
-nix --extra-experimental-features 'nix-command flakes' "$@" --allow-dirty
+if [ -n "$build_flag" ]; then
+  echo "Building the package and creating a symlink to the result in the current directory..."
+else
+  echo "Building the package without creating a symlink to the result in the current directory..."
+fi
+
+if [ -n "$build_flag" ]; then
+  nix --extra-experimental-features 'nix-command flakes' build $link_flag --out-link .nix --allow-dirty "$@"
+else
+  nix --extra-experimental-features 'nix-command flakes' "$@"
+fi
+
+
